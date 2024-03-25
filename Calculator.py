2215df6ac94b6d4739d4a3c4c333fabf6a8b3dab
Mohan_K_17 <mohank12092003@gmail.com>
2024-03-25 23:31:49 +0530

    color changed

--- Calculator.py
+++ Calculator.py
@@ -10,7 +10,7 @@ class calculate():
 
         self.root.maxsize(290, 260)
         self.root.minsize(290, 260)
-        self.root.config(bg="grey")
+        self.root.config(bg="#DBE2EF")
 
         self.resultwindow = Entry(self.root,borderwidth=5, relief=SUNKEN)
         self.resultwindow.grid(row=0,column=0,columnspan=6,pady=5)
@@ -18,43 +18,43 @@ class calculate():
         self.resultwindow.focus_set()  # Sets focus on the input text area
 
         # Buttons
-        self.button1 = Button(self.root, text="1", width=3, command=lambda:self.ins('1'),relief=RAISED,bg='light green')
+        self.button1 = Button(self.root, text="1", width=3, command=lambda:self.ins('1'),relief=RAISED,bg='#A5DD9B')
         self.button1.grid(row=1,column=0, padx=3, pady=3)
         self.button1.config(font=("Arial", 18))
 
-        self.button2 = Button(self.root, text="2", width=3, command=lambda:self.ins('2'),relief=RAISED,bg='light green')
+        self.button2 = Button(self.root, text="2", width=3, command=lambda:self.ins('2'),relief=RAISED,bg='#A5DD9B')
         self.button2.grid(row=1, column=1, padx=3, pady=3)
         self.button2.config(font=("Arial", 18))
 
-        self.button3 = Button(self.root, text="3", width=3, command=lambda:self.ins('3'),relief=RAISED,bg='light green')
+        self.button3 = Button(self.root, text="3", width=3, command=lambda:self.ins('3'),relief=RAISED,bg='#A5DD9B')
         self.button3.grid(row=1, column=2, padx=3, pady=3)
         self.button3.config(font=("Arial", 18))
 
-        self.button4 = Button(self.root, text="4", width=3, command=lambda:self.ins('4'),relief=RAISED,bg='light green')
+        self.button4 = Button(self.root, text="4", width=3, command=lambda:self.ins('4'),relief=RAISED,bg='#A5DD9B')
         self.button4.grid(row=2, column=0, padx=3, pady=3)
         self.button4.config(font=("Arial", 18))
 
-        self.button5 = Button(self.root, text="5", width=3, command=lambda:self.ins('5'),relief=RAISED,bg='light green')
+        self.button5 = Button(self.root, text="5", width=3, command=lambda:self.ins('5'),relief=RAISED,bg='#A5DD9B')
         self.button5.grid(row=2, column=1, padx=3, pady=3)
         self.button5.config(font=("Arial", 18))
 
-        self.button6 = Button(self.root, text="6", width=3, command=lambda:self.ins('6'),relief=RAISED,bg='light green')
+        self.button6 = Button(self.root, text="6", width=3, command=lambda:self.ins('6'),relief=RAISED,bg='#A5DD9B')
         self.button6.grid(row=2, column=2, padx=3, pady=3)
         self.button6.config(font=("Arial", 18))
 
-        self.button7 = Button(self.root, text="7", width=3, command=lambda:self.ins('7'),relief=RAISED,bg='light green')
+        self.button7 = Button(self.root, text="7", width=3, command=lambda:self.ins('7'),relief=RAISED,bg='#A5DD9B')
         self.button7.grid(row=3, column=0, padx=3, pady=3)
         self.button7.config(font=("Arial", 18))
 
-        self.button8 = Button(self.root, text="8", width=3, command=lambda:self.ins('8'),relief=RAISED,bg='light green')
+        self.button8 = Button(self.root, text="8", width=3, command=lambda:self.ins('8'),relief=RAISED,bg='#A5DD9B')
         self.button8.grid(row=3, column=1, padx=3, pady=3)
         self.button8.config(font=("Arial", 18))
 
-        self.button9 = Button(self.root, text="9", width=3, command=lambda:self.ins('9'),relief=RAISED,bg='light green')
+        self.button9 = Button(self.root, text="9", width=3, command=lambda:self.ins('9'),relief=RAISED,bg='#A5DD9B')
         self.button9.grid(row=3, column=2, padx=3, pady=3)
         self.button9.config(font=("Arial", 18))
 
-        self.button0 = Button(self.root, text="0", width=3, command=lambda: self.ins('0'),relief=RAISED,bg='light green')
+        self.button0 = Button(self.root, text="0", width=3, command=lambda: self.ins('0'),relief=RAISED,bg='#A5DD9B')
         self.button0.grid(row=4, column=0, padx=3, pady=3)
         self.button0.config(font=("Arial", 18))
 
